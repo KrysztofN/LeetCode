61126e4e7138efb83e8481ed0cc034cb0a1eb5b4
Kris <kris1toja@gmail.com>
2025-04-27 12:29:02 +0200

    added 2140 leet

---
+++ python/medium/2140.py
@@ -0,0 +1,21 @@
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        
+        cache = [0] * len(questions)
+
+        def backtrack(i):
+            if i >= len(questions):
+                return 0
+
+            if cache[i]:
+                return cache[i]
+            
+            points, brainpower = questions[i]
+
+            cache[i] =  max(
+                backtrack(i + 1),
+                points + backtrack(i + brainpower + 1)
+            )
+            return cache[i]
+
+        return backtrack(0)
